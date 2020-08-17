6bc025455979e2abdf1b86317e280f9ac4dc15af
JEALG <jalain.gosse@icloud.com>
2020-08-17 11:58:18 +0200

    Font version 5

--- plugin_info/configuration.php
+++ plugin_info/configuration.php
@@ -23,72 +23,78 @@ if (!isConnect()) {
 }
 ?>
 <form class="form-horizontal">
-    <fieldset>
-    	<legend>{{Lien des données Météo pour des sites tiers}}</legend>
-        <div class="form-group">
-			<label class="col-lg-4 control-label"><a href="http://ma.station-meteo.com/import/new" target="_new"><img src="http://www.station-meteo.com/img/logo/station-meteo-120-60.gif" alt="Station-meteo" border="0" height="20"/></a> ma.station-meteo.com : {{Choisir format}} "awekas.txt"</label>
-            <div class="col-lg-8 url_api">
-            <?php
+	<fieldset>
+		<legend>{{Lien des données Météo pour des sites tiers}}</legend>
+		<div class="form-group">
+			<label class="col-lg-4 control-label"><a href="http://ma.station-meteo.com/import/new" target="_new"><img src="http://www.station-meteo.com/img/logo/station-meteo-120-60.gif" alt="Station-meteo" border="0" height="20" /></a> ma.station-meteo.com : {{Choisir format}} "awekas.txt"</label>
+			<div class="col-lg-8 url_api">
+				<?php
 				echo network::getNetworkAccess('external') . "/plugins/publiemeteo/core/php/publiemeteo.php?api=" . config::byKey('api', 'publiemeteo') . "&format=awekas.at";
-			?></div>
-        </div>
-        <div class="form-group">
-            <label class="col-lg-4 control-label"><a href="http://www.awekas.at" target="_new"><img src="http://www.awekas.at/images/awekas-logo-kl.jpg" alt="Awekas" border="0" height="20"/></a> awekas.at : {{Choisir format}} "WSWIN"</label>
-            <div class="col-lg-6 url_api">
-            <?php
+				?></div>
+		</div>
+		<div class="form-group">
+			<label class="col-lg-4 control-label"><a href="http://www.awekas.at" target="_new"><img src="http://www.awekas.at/images/awekas-logo-kl.jpg" alt="Awekas" border="0" height="20" /></a> awekas.at : {{Choisir format}} "WSWIN"</label>
+			<div class="col-lg-6 url_api">
+				<?php
 				echo network::getNetworkAccess('external') . "/plugins/publiemeteo/core/php/publiemeteo.php?api=" . config::byKey('api', 'publiemeteo') . "&format=awekas.at";
-			?></div>
-        </div>
-        <div class="form-group">
-            <label class="col-lg-4 control-label"><a href="http://www.wunderground.com" target="_new"><img src="https://icons.wxug.com/graphics/wu2/logo_130x80.png" alt="Wunderground" border="0" height="20"/></a> wunderground.com : </label>
-            <div class="col-lg-1">
-				Id : 
+				?></div>
+		</div>
+		<div class="form-group">
+			<label class="col-lg-4 control-label"><a href="http://www.wunderground.com" target="_new"><img src="https://icons.wxug.com/graphics/wu2/logo_130x80.png" alt="Wunderground" border="0" height="20" /></a> wunderground.com : </label>
+			<div class="col-lg-1">
+				Id :
 			</div>
 			<div class="col-lg-2">
 				<input class="configKey form-control" data-l1key="wunderground_id" placeholder="ID as registered by wunderground.com">
 			</div>
-            <div class="col-lg-1">
-				Password : 
+			<div class="col-lg-1">
+				Password :
 			</div>
 			<div class="col-lg-2">
 				<input type="password" class="configKey form-control" data-l1key="wunderground_password" placeholder="PASSWORD registered with this ID">
 			</div>
-        </div>
+		</div>
 		<div class="form-group">
 			<label class="col-lg-4 control-label">{{Clef API}}</label>
 			<div class="col-lg-5">
-				<a class="btn btn-success" id="bt_resetkey"><i class="fa fa-refresh"></i> {{Reset}}</a> {{Il sera nécessaire de reconfigurer les URLs sur les sites Internet.}}
+				<a class="btn btn-success" id="bt_resetkey"><i class="fas fa-refresh"></i> {{Reset}}</a> {{Il sera nécessaire de reconfigurer les URLs sur les sites Internet.}}
 			</div>
 		</div>
-    </fieldset>
+	</fieldset>
 </form>
 <div id='div_ResetKeyResult' style="display: none;"></div>
-	<script>
-		$('#bt_resetkey').on('click', function () {
-			bootbox.confirm('{{Etes-vous sûr de vouloir forcer la génération d\'une nouvelle clef API ?}}', function (result) {
-				if (result) {
-					$.ajax({// fonction permettant de faire de l'ajax
-						type: "POST", // methode de transmission des données au fichier php
-						url: "plugins/publiemeteo/core/ajax/publiemeteo.ajax.php", // url du fichier php
-						data: {
-							action: "ResetKey",
-						},
-						dataType: 'json',
-						error: function(request, status, error) {
-							handleAjaxError(request, status, $('#div_ResetKeyResult'));
-						},
-						success: function(data) { // si l'appel a bien fonctionné
-							if (data.state != 'ok') {
-								$('#div_ResetKeyResult').showAlert({message: data.result, level: 'danger'});
-							} else {
-								$('.url_api').each(function(){
-									$(this).value($(this).value().replace(/api=[a-z]*&/i, "api="+data.result+"&"));
-								});
-								$('#div_ResetKeyResult').showAlert({message: '{{Mise a jour de clef API OK}}', level: 'success'});
-							}
+<script>
+	$('#bt_resetkey').on('click', function() {
+		bootbox.confirm('{{Etes-vous sûr de vouloir forcer la génération d\'une nouvelle clef API ?}}', function(result) {
+			if (result) {
+				$.ajax({ // fonction permettant de faire de l'ajax
+					type: "POST", // methode de transmission des données au fichier php
+					url: "plugins/publiemeteo/core/ajax/publiemeteo.ajax.php", // url du fichier php
+					data: {
+						action: "ResetKey",
+					},
+					dataType: 'json',
+					error: function(request, status, error) {
+						handleAjaxError(request, status, $('#div_ResetKeyResult'));
+					},
+					success: function(data) { // si l'appel a bien fonctionné
+						if (data.state != 'ok') {
+							$('#div_ResetKeyResult').showAlert({
+								message: data.result,
+								level: 'danger'
+							});
+						} else {
+							$('.url_api').each(function() {
+								$(this).value($(this).value().replace(/api=[a-z]*&/i, "api=" + data.result + "&"));
+							});
+							$('#div_ResetKeyResult').showAlert({
+								message: '{{Mise a jour de clef API OK}}',
+								level: 'success'
+							});
 						}
-					});
-				}
-			});
+					}
+				});
+			}
 		});
-	</script>
+	});
+</script>
